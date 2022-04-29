65f49b7234be502ad0735e7b194dffcaba313649
Maksat <maksiqkgz@gmail.com>
2022-04-30 05:48:57 +0600

    add forms, crud based on fbv

--- places/views.py
+++ places/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Place
+from .forms import PlaceForm
 
 
 def places(request):
     places_objects = Place.objects.all() #Select * From Place
 
-    return render(request, 'places.html', {'places': places_objects})
+    return render(request, 'places/places.html', {'places': places_objects})
+
+def create_place(request):
+    if request.method == 'POST':
+        place_form = PlaceForm(request.POST)
+        if place_form.is_valid():
+            place_form.save()
+            return redirect(places)
+
+    place_form = PlaceForm()
+    return render(request, 'places/form.html', {'place_form': place_form})
+
+def place(request, id):
+    place_object = Place.objects.get(id=id)
+    return render(request, 'places/place.html', {'place_object': place_object})
+
+def edit_place(request, id):
+    place_object = Place.objects.get(id=id)
+
+    if request.method == 'POST':
+        place_form = PlaceForm(data=request.POST, instance=place_object)
+        if place_form.is_valid():
+            place_form.save()
+            return redirect(place, id=id)
+
+    place_form = PlaceForm(instance=place_object)
+    return render(request, 'places/form.html', {'place_form': place_form})
+
+def delete_place(request, id):
+    place_object = Place.objects.get(id=id)
+    place_object.delete()
+    return redirect(places)
